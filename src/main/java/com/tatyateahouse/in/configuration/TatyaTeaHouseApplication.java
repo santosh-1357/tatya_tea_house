578814de3fffe079f1105ac70e9d4773fd468167
santosh_wakhare <santosh.wakhare@taxgenie.in>
2023-03-08 13:35:45 +0530

    added project files

---
+++ src/main/java/com/tatyateahouse/in/configuration/TatyaTeaHouseApplication.java
@@ -0,0 +1,13 @@
+package com.tatyateahouse.in.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TatyaTeaHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TatyaTeaHouseApplication.class, args);
+	}
+
+}
